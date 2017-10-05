f9542338cc5954a3bb57246bae2b9a833a1f13e6
Antti <anttis.pitkanen@gmail.com>
2017-10-05 15:14:03 +0300

    Add Introduction section.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import Lander from './components/Lander';
 import Filler from './components/Filler';
+import Lander from './components/Lander';
+import Introduction from './components/Introduction';
 
 const App = () => (
     <div className="App">
         <Lander />
+        <Introduction />
         <Filler
             textSource="einstein.md"
             quotedPerson="einstein"
---
+++ src/components/Introduction.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import MarkdownParser from '../MarkdownParser';
+import { videoEmbed } from '../videoEmbed';
+import './styles/Introduction.css';
+
+const IntroEmbed = videoEmbed({
+    url: 'embeds/introduction.md',
+    className: 'youtube-embed',
+}, {
+    origWidth: 560,
+    origHeight: 315,
+    sideMargin: 20,
+    aspectRatio: 0.5625
+})(MarkdownParser);
+
+const Introduction = () => (
+    <div className="Introduction">
+        <MarkdownParser
+            url="text/introduction-1.md"
+            className="introduction"
+        />
+
+        <IntroEmbed />
+
+        <MarkdownParser
+            url="text/introduction-2.md"
+            className="introduction"
+        />
+    </div>
+);
+
+export default Introduction;
